a48e27295ebfe40d3c900e0ee163fe552d5c459d
Jesse Dillman <jesse@jdillman.com>
2016-10-11 17:46:40 -0500

    logfile name

--- logsession.php
+++ logsession.php
@@ -3,7 +3,7 @@
 // Get and format data
 $data = $_GET["data"];
 
-$time = date("d-M-Y_H:i\(e\)");
+$time = date("d-M-Y_H:i:s\(e\)");
 $logfile = fopen("logs/".$time, "w") or die("Unable to open file!");
 fwrite($logfile, $data);
 fclose($logfile);
